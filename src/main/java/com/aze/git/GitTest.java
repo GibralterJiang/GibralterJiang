f04568244fc3f3564de21fe7ac2390e19a6dd869
GibralterJiang <106289822+GibralterJiang@users.noreply.github.com>
2022-07-25 15:59:01 +0800

    pull commit

--- src/main/java/com/aze/git/GitTest.java
+++ src/main/java/com/aze/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("hello hot-fix!");
         System.out.println("hello master!");
         System.out.println("push test!");
+        System.out.println("pull test!");
     }
 }
