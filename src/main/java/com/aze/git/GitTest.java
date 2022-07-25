51f144b9d3451e76abb8517412912e60bc8ef2d4
lyz <a3119739559@outlook.com>
2022-07-25 15:21:19 +0800

    hot-fix commit2

--- src/main/java/com/aze/git/GitTest.java
+++ src/main/java/com/aze/git/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("hello git2!");
         System.out.println("hello git3!");
         System.out.println("hello hot-fix!");
+        System.out.println("hello hot-fix!");
     }
 }
